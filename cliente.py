b639d4559dd7ae01eb7f146d20ae87bcd1213097
mgabimedeiros <mgms@cin.ufpe.br>
2026-09-23 13:28:29 -0300

    Recebe arquivo, divide no tamanho suporta e falta terminar a parte da devolução do arquivo para o cliente

--- cliente.py
+++ cliente.py
@@ -1,12 +1,46 @@
 import socket
+import sys #leitura de parâmetros no terminal
+import os #lida com arquivos e caminhos
 
 HOST = "127.0.0.1"
 PORTA = 5000
+TAMANHO_PCT = 1024
+
+#lê o nome do arquivo digitado no terminal ("python cliente.py texte.txt")
+#o sys.argv gera uma lista com tudo que foi escrito no terminal
+if len(sys.argv) != 2: #se o tamanho da lista ta diferente de 2 você esqueceu algo
+    print("Uso: python cliente.py <nome_do_arquivo>") #com essa formatação -> sys.argv[1] = <nome_do_arquivo>
+    sys.exit(1)
+
+caminho = sys.argv[1]  #assimila o caminho ao nome do arquivo escrito no terminal
+
+#verifica se o arquivo existe
+if not os.path.isfile(caminho):
+    print(f"[CLIENTE] Arquivo '{caminho}' não encontrado")
+    sys.exit(1)
+
+nome_arquivo = os.path.basename(caminho)  #pega o nome do arquivo
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-mensagem = "Olá servidor!".encode()  #converter o texto em bytes (comunicação UDP)
-sock.sendto(mensagem, (HOST,PORTA))
+#Primeio pacote informa o nome e indica que o que chega agora faz parte desse pacote
+sock.sendto(f"INICIO|{nome_arquivo}".encode(), (HOST,PORTA)) #o encode converte o texto em bytes
+print(f"[CLIENTE] Enviando arquivo '{nome_arquivo}'...")
 
-print("[CLIENTE] Mensagem enviada")
+#Pacotes de dados (blocos de até 1024 bytes)
+numero = 0
+with open (caminho, "rb") as f:  #abre o arquivo em read binary (rb)
+    while True:
+        bloco = f.read(TAMANHO_PCT) #lê até 1024 bytes por vez
+        if not bloco:
+            break
+        sock.sendto(bloco, (HOST,PORTA))
+        numero +=1
+        print(f"[CLIENTE] Pacote {numero} enviado ({len(bloco)} bytes)")
+
+#Último pacote, encerra o recebimento desse pacote
+sock.sendto(b"FIM", (HOST,PORTA))
+print(f"[CLIENTE] Envio concluído: {numero} pacotes.")
+
+
 sock.close()
--- servidor.py
+++ servidor.py
@@ -1,8 +1,13 @@
 import socket
+import os
 
 HOST = "127.0.0.1" #este computador
 PORTA = 5000
 TAMANHO_BUFFER = 2048 #1024x2 (folga)
+PASTA_DESTINO = "arquivos_servidor" #onde os arquivos recebidos serão salvos
+
+#cria a pasta de destino, caso não exista
+os.makedirs(PASTA_DESTINO, exist_ok=True) 
 
 #criação do socket UDP
 #AF_INET - endereço IPv4
@@ -14,11 +19,41 @@ sock.bind((HOST,PORTA))
 
 print(f"[SERVIDOR] escutando em {HOST}:{PORTA}...")
 
+nome_arquivo_atual = None
+blocos = []
 try:
     while True: #mantem o servidor sempre ligado
-        #espera a chegada de algum pacote, se não chega ele trava
+        #espera a chegada de algum pacote, se não chega ele fica travado esperando
         dados, endereco = sock.recvfrom(TAMANHO_BUFFER)
-        print(f"[SERVIDOR] recebi {len(dados)} bytes de {endereco}:{dados}")
+
+        if dados.startswith(b"INICIO|"):
+            nome_arquivo_atual = dados.decode().split("|",1)[1]
+            blocos = [] #zera a lista para um novo arquivo
+            print(f"[SERVIDOR] Início do arquivo '{nome_arquivo_atual}' vindo de {endereco}")
+        elif dados == b"FIM":
+            #junta todos os pedaços em uma sequência única de bytes
+            conteudo_completo = b"".join(blocos)
+
+            #salva com o prefixo "servidor_"
+            nome_salvar = f"servidor_{nome_arquivo_atual}"
+            caminho_salvar = os.path.join(PASTA_DESTINO, nome_salvar)
+
+            with open(caminho_salvar, "wb") as f:
+                f.write(conteudo_completo)
+
+            print(f"[SERVIDOR] Arquivo salvo como '{caminho_salvar}'"
+                  f"({len(conteudo_completo)} bytes, {len(blocos)} pacotes)")
+
+            #Retorno do arquivo para o cliente
+            print(f"[SERVIDOR] Devolvendo {nome_arquivo_atual} para {endereco}...") #endereço do cliente, de onde veio os dados
+            sock.sendto(f"INICIO|{nome_arquivo_atual}".encode(), endereco)
+            
+        else:  #é um bloco do arquivo
+            blocos.append(dados)
+            print(f"[SERVIDOR] Pacote {len(blocos)} recebido ({len(dados)} bytes)")
+
+
+
 except KeyboardInterrupt:
     print("\n[SERVIDOR] Encerrado pelo usuário")
 finally:
