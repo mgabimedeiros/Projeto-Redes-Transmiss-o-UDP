c128407e3fa6676842c8ea837025f53b31060228
mgabimedeiros <mgms@cin.ufpe.br>
2026-09-23 21:34:16 -0300

    Implementa envio, armazenamento e devolução de arquivos via UDP fragmentado

--- cliente.py
+++ cliente.py
@@ -5,14 +5,18 @@ import os #lida com arquivos e caminhos
 HOST = "127.0.0.1"
 PORTA = 5000
 TAMANHO_PCT = 1024
+TAMANHO_BUFFER = 2048
+PASTA_DESTINO = "arquivos_cliente" #pasta para guardar arquivos recebidos do servidor
+
 
 #lê o nome do arquivo digitado no terminal ("python cliente.py texte.txt")
 #o sys.argv gera uma lista com tudo que foi escrito no terminal
 if len(sys.argv) != 2: #se o tamanho da lista ta diferente de 2 você esqueceu algo
-    print("Uso: python cliente.py <nome_do_arquivo>") #com essa formatação -> sys.argv[1] = <nome_do_arquivo>
+    print("Uso: python cliente.py <arquivos_teste/nome_do_arquivo>") #com essa formatação -> sys.argv[1] = <nome_do_arquivo>
     sys.exit(1)
 
 caminho = sys.argv[1]  #assimila o caminho ao nome do arquivo escrito no terminal
+
 
 #verifica se o arquivo existe
 if not os.path.isfile(caminho):
@@ -20,8 +24,9 @@ if not os.path.isfile(caminho):
     sys.exit(1)
 
 nome_arquivo = os.path.basename(caminho)  #pega o nome do arquivo
+os.makedirs(PASTA_DESTINO, exist_ok=True) #cria a pasta destino
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #estabelece conexão
 
 #Primeio pacote informa o nome e indica que o que chega agora faz parte desse pacote
 sock.sendto(f"INICIO|{nome_arquivo}".encode(), (HOST,PORTA)) #o encode converte o texto em bytes
@@ -34,7 +39,7 @@ with open (caminho, "rb") as f:  #abre o arquivo em read binary (rb)
         bloco = f.read(TAMANHO_PCT) #lê até 1024 bytes por vez
         if not bloco:
             break
-        sock.sendto(bloco, (HOST,PORTA))
+        sock.sendto(bloco, (HOST,PORTA)) #envia o bloco para o servidor
         numero +=1
         print(f"[CLIENTE] Pacote {numero} enviado ({len(bloco)} bytes)")
 
@@ -43,4 +48,35 @@ sock.sendto(b"FIM", (HOST,PORTA))
 print(f"[CLIENTE] Envio concluído: {numero} pacotes.")
 
 
+#inicia o recebimento do pacote
+print("[CLIENTE] Aguardando devolução do servidor...")
+
+nome_recebido = None
+blocos_recebidos = []
+
+while True:
+    dados, endereco = sock.recvfrom(TAMANHO_BUFFER) #recebe o pacote, respeitando o tamanho do buffer
+
+    if dados.startswith(b"INICIO|"): #verifica se é o início do arquivo (faz o mesmo processo que é feito no "servidor.py")
+        nome_recebido = dados.decode().split("|", 1)[1]
+        blocos_recebidos = []
+        print(f"[CLIENTE] Recebendo devolução do arquivo '{nome_recebido}'...")
+
+    elif dados == b"FIM":
+        conteudo_completo = b"".join(blocos_recebidos)
+        nome_salvar = f"cliente_{nome_recebido}"
+        caminho_salvar = os.path.join(PASTA_DESTINO, nome_salvar)
+
+        with open(caminho_salvar, "wb") as f:
+            f.write(conteudo_completo)
+
+        print(f"[CLIENTE] Arquivo salvo como '{caminho_salvar}' "
+              f"({len(conteudo_completo)} bytes, {len(blocos_recebidos)} pacotes)")
+        break  # terminou, pode sair do loop
+
+    else:
+        blocos_recebidos.append(dados)
+        print(f"[CLIENTE] Pacote {len(blocos_recebidos)} recebido ({len(dados)} bytes)")
+
 sock.close()
+
--- servidor.py
+++ servidor.py
@@ -4,6 +4,7 @@ import os
 HOST = "127.0.0.1" #este computador
 PORTA = 5000
 TAMANHO_BUFFER = 2048 #1024x2 (folga)
+TAMANHO_BLOCO = 1024
 PASTA_DESTINO = "arquivos_servidor" #onde os arquivos recebidos serão salvos
 
 #cria a pasta de destino, caso não exista
@@ -17,7 +18,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 #associa o socket ao endereço e a porta
 sock.bind((HOST,PORTA))
 
-print(f"[SERVIDOR] escutando em {HOST}:{PORTA}...")
+print(f"[SERVIDOR] escutando em {HOST}:{PORTA}...") #sinaliza que o servidor está ativo
 
 nome_arquivo_atual = None
 blocos = []
@@ -26,32 +27,41 @@ try:
         #espera a chegada de algum pacote, se não chega ele fica travado esperando
         dados, endereco = sock.recvfrom(TAMANHO_BUFFER)
 
-        if dados.startswith(b"INICIO|"):
+        if dados.startswith(b"INICIO|"):   #em cliente.py o primeiro pacote do arquivo é enviado com "INICIO|" -> identifica a chegada de um novo arquivo
             nome_arquivo_atual = dados.decode().split("|",1)[1]
             blocos = [] #zera a lista para um novo arquivo
-            print(f"[SERVIDOR] Início do arquivo '{nome_arquivo_atual}' vindo de {endereco}")
-        elif dados == b"FIM":
+            print(f"[SERVIDOR] Início do arquivo '{nome_arquivo_atual}' vindo de {endereco}") #sinaliza nome do arquivo e o endereço que veio
+        elif dados == b"FIM": #chegou ao fim do arquivo
             #junta todos os pedaços em uma sequência única de bytes
             conteudo_completo = b"".join(blocos)
 
             #salva com o prefixo "servidor_"
             nome_salvar = f"servidor_{nome_arquivo_atual}"
-            caminho_salvar = os.path.join(PASTA_DESTINO, nome_salvar)
+            caminho_salvar = os.path.join(PASTA_DESTINO, nome_salvar) #salva na pasta "arquivos_servidor" o arquivo de destino de todos os pacotes(blocos)
 
             with open(caminho_salvar, "wb") as f:
-                f.write(conteudo_completo)
+                f.write(conteudo_completo) #escreve todo o conteudo no arquivo
 
-            print(f"[SERVIDOR] Arquivo salvo como '{caminho_salvar}'"
+            print(f"[SERVIDOR] Arquivo salvo como '{caminho_salvar}'" 
                   f"({len(conteudo_completo)} bytes, {len(blocos)} pacotes)")
 
-            #Retorno do arquivo para o cliente
+            #quando termina de receber e salva o arquivo, inicia o envio para o cliente
             print(f"[SERVIDOR] Devolvendo {nome_arquivo_atual} para {endereco}...") #endereço do cliente, de onde veio os dados
-            sock.sendto(f"INICIO|{nome_arquivo_atual}".encode(), endereco)
-            
-        else:  #é um bloco do arquivo
-            blocos.append(dados)
-            print(f"[SERVIDOR] Pacote {len(blocos)} recebido ({len(dados)} bytes)")
+            sock.sendto(f"INICIO|{nome_arquivo_atual}".encode(), endereco) #envia com a identificaçaõ "INICIO|" para o endereço do cliente
+            numero_envio = 0
+            for i in range(0, len(conteudo_completo), TAMANHO_BLOCO):  #devolução de cada pedaço do arquivo respeitando o limite de 1024 bytes
+                bloco = conteudo_completo[i:i + TAMANHO_BLOCO]
+                sock.sendto(bloco, endereco)
+                numero_envio += 1
+                print(f"[SERVIDOR] Pacote {numero_envio} devolvido ({len(bloco)} bytes)")
 
+            sock.sendto(b"FIM", endereco) #quando sai do for envia o "FIM" para avisar que o arquivo terminou
+            print(f"[SERVIDOR] Devolução concluída: {numero_envio} pacotes.")
+
+
+        else:  #ainda está recebendo blocos do arquivo
+            blocos.append(dados) #adiciona o pedaço na lista de blocos
+            print(f"[SERVIDOR] Pacote {len(blocos)} recebido ({len(dados)} bytes)")
 
 
 except KeyboardInterrupt:
